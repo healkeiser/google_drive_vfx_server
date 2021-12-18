0bfb7a8c2473a608f656c93c3cb3b54d6a0e031a
Valentin Beaumont <valentin.onze@gmail.com>
2021-12-18 15:15:46 +0000

    Added After Effects

--- .config/pipeline/after_effects/move_plugins.py
+++ .config/pipeline/after_effects/move_plugins.py
@@ -16,9 +16,11 @@ if os.path.isdir(path_adobe):
             list_afterEffects.append(software)
             list_afterEffects = sorted(list_afterEffects)
     recent_afterEffects = list_afterEffects[-1]
+    # ------ TODO: Give user admin privileges to copy in After Effects installation folder
     # ------ Copy folders from .config to After Effects installation
     copy_tree(path_plugins, os.path.join(path_adobe, recent_afterEffects, 'Plug-ins'))
     copy_tree(paths_scripts, os.path.join(path_adobe, recent_afterEffects, 'Scripts'))
+    print('Success')
 else:
     print('No After Effects installation detected')
 
