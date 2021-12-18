f16e082be6bb087a9ad23bacad1c94eba8d9c02e
Valentin Beaumont <valentin.onze@gmail.com>
2021-12-18 15:12:10 +0000

    Added After Effects

---
+++ .config/pipeline/after_effects/move_plugins.py
@@ -0,0 +1,25 @@
+import os
+from distutils.dir_util import copy_tree
+
+user = os.getenv('USERNAME')
+pipelineRoot = os.getenv('PIPELINE_ROOT')
+path_plugins = pipelineRoot + '/.config/pipeline/after_effects/Support Files/Plug-ins'
+paths_scripts = pipelineRoot + '/.config/pipeline/after_effects/Support Files/Scripts'
+path_adobe = 'C:/Program Files/Adobe/'
+
+if os.path.isdir(path_adobe):
+    # ------ Check installations of After Effects and copy files to the most recent one
+    list_adobe = os.listdir(path_adobe)
+    list_afterEffects = []
+    for software in list_adobe:
+        if software.startswith('Adobe After Effects'):
+            list_afterEffects.append(software)
+            list_afterEffects = sorted(list_afterEffects)
+    recent_afterEffects = list_afterEffects[-1]
+    # ------ Copy folders from .config to After Effects installation
+    copy_tree(path_plugins, os.path.join(path_adobe, recent_afterEffects, 'Plug-ins'))
+    copy_tree(paths_scripts, os.path.join(path_adobe, recent_afterEffects, 'Scripts'))
+else:
+    print('No After Effects installation detected')
+
+
