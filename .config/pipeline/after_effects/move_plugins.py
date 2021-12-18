8836db0568af86a22deb198e00cb604bed0cab3e
Valentin Beaumont <valentin.onze@gmail.com>
2021-12-18 16:24:53 +0000

    Automatic After Effects moving

--- .config/pipeline/after_effects/move_plugins.py
+++ .config/pipeline/after_effects/move_plugins.py
@@ -1,27 +1,37 @@
+import ctypes
 import os
+import sys
 from distutils.dir_util import copy_tree
 
-user = os.getenv('USERNAME')
 pipelineRoot = os.getenv('PIPELINE_ROOT')
 path_plugins = pipelineRoot + '/.config/pipeline/after_effects/Support Files/Plug-ins'
 paths_scripts = pipelineRoot + '/.config/pipeline/after_effects/Support Files/Scripts'
 path_adobe = 'C:/Program Files/Adobe/'
 
-if os.path.isdir(path_adobe):
-    # ------ Check installations of After Effects and copy files to the most recent one
-    list_adobe = os.listdir(path_adobe)
-    list_afterEffects = []
-    for software in list_adobe:
-        if software.startswith('Adobe After Effects'):
-            list_afterEffects.append(software)
-            list_afterEffects = sorted(list_afterEffects)
-    recent_afterEffects = list_afterEffects[-1]
-    # ------ TODO: Give user admin privileges to copy in After Effects installation folder
-    # ------ Copy folders from .config to After Effects installation
-    copy_tree(path_plugins, os.path.join(path_adobe, recent_afterEffects, 'Plug-ins'))
-    copy_tree(paths_scripts, os.path.join(path_adobe, recent_afterEffects, 'Scripts'))
-    print('Success')
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
+if is_admin():
+    if os.path.isdir(path_adobe):
+        # ------ Check installations of After Effects and copy files to the most recent one
+        list_adobe = os.listdir(path_adobe)
+        list_afterEffects = []
+        for software in list_adobe:
+            if software.startswith('Adobe After Effects'):
+                list_afterEffects.append(software)
+                list_afterEffects = sorted(list_afterEffects)
+        recent_afterEffects = list_afterEffects[-1]
+        # ------ Copy folders from .config to After Effects installation
+        copy_tree(path_plugins, os.path.join(path_adobe, recent_afterEffects, 'Plug-ins'))
+        copy_tree(paths_scripts, os.path.join(path_adobe, recent_afterEffects, 'Scripts'))
+        print('Success')
+    else:
+        print('No After Effects installation detected')
 else:
-    print('No After Effects installation detected')
-
-
+    # ------ Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
