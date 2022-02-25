b5302fe99a1a49a802d46d7533fc3d289ca471da
Valentin Beaumont <valentin.onze@gmail.com>
2022-02-25 16:05:34 +0000

    Cleanup

--- .config/pipeline/after_effects/move_plugins.py
+++ .config/pipeline/after_effects/move_plugins.py
@@ -3,9 +3,9 @@ import os
 import sys
 from distutils.dir_util import copy_tree
 
-pipelineRoot = os.getenv("PIPELINE_ROOT")
-path_plugins = pipelineRoot + "/.config/pipeline/after_effects/Support Files/Plug-ins"
-paths_scripts = pipelineRoot + "/.config/pipeline/after_effects/Support Files/Scripts"
+pipeline_root = os.getenv("PIPELINE_ROOT")
+path_plugins = f"{pipeline_root}/.config/pipeline/after_effects/Support Files/Plug-ins"
+paths_scripts = f"{pipeline_root}/.config/pipeline/after_effects/Support Files/Scripts"
 path_adobe = "C:/Program Files/Adobe/"
 
 
@@ -21,12 +21,15 @@ if is_admin():
         # ------ Check installations of After Effects and copy files to the most recent one
         list_adobe = os.listdir(path_adobe)
         list_afterEffects = []
+
         for software in list_adobe:
             if software.startswith("Adobe After Effects"):
                 list_afterEffects.append(software)
                 list_afterEffects = sorted(list_afterEffects)
+
         recent_afterEffects = list_afterEffects[-1]
-        print("Copying to " + recent_afterEffects + " installation folder...")
+        print(f"Copying to {recent_afterEffects} installation folder...")
+
         # ------ Copy folders from .config to After Effects installation
         path_plugins_afterEffects = os.path.join(
             path_adobe, recent_afterEffects, "Support Files", "Plug-ins"
@@ -36,6 +39,7 @@ if is_admin():
         ).replace("\\", "/")
         copy_tree(path_plugins.replace("\\", "/"), path_plugins_afterEffects)
         copy_tree(paths_scripts.replace("\\", "/"), path_scripts_afterEffects)
+
         # ------ Open the Plug-ins and Scripts folders to check files freshly copied
         os.startfile(path_plugins_afterEffects)
         os.startfile(path_scripts_afterEffects)
